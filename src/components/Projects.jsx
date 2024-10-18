c6bceae3f6dcedd63540659394bb18fc01a6d7e0
dhruv14122004 <dhruv.sharma122004@gmail.com>
2024-10-18 17:27:15 +0530

    Add new images for Hotel Management System and Forest Fire Detector

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,14 @@
 import { motion } from 'framer-motion';
 import sports from '../assets/sport.png'
 import event from '../assets/event.png'
-
+import forrestfire from '../assets/forrestfir.jpeg'
+import hotel from '../assets/hotel.png'
 
 const projectData = [
   { id: 1, title: 'Tennis Ball Detection System', description: 'Our project focuses on developing a robust Tennis Ball Detection System using YOLOv8 for real-time object detection. We converted videos into frames for analysis and trained models for ball detection, player identification, and court detection under various conditions. This project highlights our collaborative effort in applying cutting-edge machine learning techniques to enhance sports analytics.', img:sports },
   { id: 2, title: 'Event Management System', description: 'The system allows clubs to input event and workshop details, streamlining resource allocation for smoother event planning. Automation ensures efficient resource utilization, benefiting both organizers and participants. Collaboration between clubs is enhanced, optimizing resource allocation for better efficiency.', img:event},
-  { id: 3, title: 'Hotel Management System', description: 'The system integrates both a standard calculator and  a price calculator, boosting its functionality. It also features a dynamic billing system, automating price calculations and ensuring precise invoicing for enhanced efficiency.', img:sports },
-  { id: 4, title: 'Forest Fire Detector', description: 'Developed a comprehensive forest fire detection system using Arduino, aimed at early identification and prevention of potential wildfires. The project involved the integration of varioussensors and communication modules to ensure real-time monitoring and timely response.', img:sports }
+  { id: 3, title: 'Hotel Management System', description: 'The system integrates both a standard calculator and  a price calculator, boosting its functionality. It also features a dynamic billing system, automating price calculations and ensuring precise invoicing for enhanced efficiency.', img:hotel },
+  { id: 4, title: 'Forest Fire Detector', description: 'Developed a comprehensive forest fire detection system using Arduino, aimed at early identification and prevention of potential wildfires. The project involved the integration of varioussensors and communication modules to ensure real-time monitoring and timely response.', img:forrestfire}
 ];
 
 const Projects = () => {
