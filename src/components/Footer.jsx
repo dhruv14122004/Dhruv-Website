e95685b5d2cf13aef1443acd4c92bfc830589595
dhruv14122004 <dhruv.sharma122004@gmail.com>
2024-10-18 17:48:00 +0530

    Update social links style in Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,10 @@ const Footer = () => {
       
       <div className="flex justify-center space-x-4 mt-4">
         {/* Social Links */}
-        <a href="mailto:dhruv.sharma122004@gmail.com" className="hover:text-gray-400">Email</a>
-        <a href="https://www.linkedin.com/in/dhruv-sharma-142932252/" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400">LinkedIn</a>
-        <a href="https://github.com/dhruv14122004" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400">GitHub</a>
-        <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400">Twitter</a>
+        <a href="mailto:dhruv.sharma122004@gmail.com" className="hover:text-red-500">Email</a>
+        <a href="https://www.linkedin.com/in/dhruv-sharma-142932252/" target="_blank" rel="noopener noreferrer" className="hover:text-red-500">LinkedIn</a>
+        <a href="https://github.com/dhruv14122004" target="_blank" rel="noopener noreferrer" className="hover:text-red-500ed">GitHub</a>
+        <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer" className="hover:text-red-500">Twitter</a>
       </div>
 
       <p className="mt-6 text-sm text-gray-500">
