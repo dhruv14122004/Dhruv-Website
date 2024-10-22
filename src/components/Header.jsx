e4f0c4ac19a41136ff28a9946892c5225f4259cf
dhruv14122004 <dhruv.sharma122004@gmail.com>
2024-10-22 20:50:35 +0530

    Update Header component with link to portfolio website

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,9 @@ import { TbCircleLetterDFilled } from "react-icons/tb";
 const Header = () => {
     return (
       <div id = "Home" className="px-10 py-4 text-xl">
-        <TbCircleLetterDFilled className="text-[40px] transition-transform duration-300 hover:scale-150"/>
+        <a href="https://dhruv14122004.github.io/dhruv_portfolio_website/" target="_blank">
+          <TbCircleLetterDFilled className="text-[40px] transition-transform duration-300 hover:scale-150"/>
+        </a>
     </div>
     );
   };
